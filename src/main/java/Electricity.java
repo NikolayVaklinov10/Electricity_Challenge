29da9cb7cdb677a8eaead46cd3dddd75e460b033
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-11-25 14:22:38 +0000

    simple application to calculate the average of several numbers

---
+++ src/main/java/Electricity.java
@@ -0,0 +1,41 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.Scanner;
+import java.util.stream.DoubleStream;
+
+public class Electricity {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Enter how many numbers you want to use to calculate the average");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double number = scanner.nextDouble();
+        int count = 0;
+        for (double j = 0; j < number; j++) {
+            count++;
+        }
+
+        double[] arr = new double[count];
+
+        System.out.println("\nEnter numbers now\n");
+        for (double i = 0; i < number; i++) {
+            System.out.println("Enter number " + (i + 1) + ":");
+            arr[(int) i] = scanner.nextDouble();
+        }
+
+        DoubleStream streamOfNumbers = Arrays.stream(arr);
+        OptionalDouble averageOfNumbers = streamOfNumbers.distinct().average();
+
+        System.out.println("\nAverage of Numbers: " + averageOfNumbers);
+
+
+
+
+
+    }
+}
